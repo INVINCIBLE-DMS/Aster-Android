de44dd62720892fc063aefabcf5787031ea2b197
uielPark <parkuiery@gmail.com>
2023-12-24 15:19:40 +0900

    🌱 :: sharedPreference 세팅

--- Aster/app/src/main/java/com/aster/android/util/SharedPreferenceUtil.kt
+++ Aster/app/src/main/java/com/aster/android/util/SharedPreferenceUtil.kt
@@ -1,4 +1,32 @@
 package com.aster.android.util
 
-class SharedPreferencetil {
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
+
+fun initPref(
+    context: Context,
+): SharedPreferences = context.getSharedPreferences("user", MODE_PRIVATE)
+
+fun putPref(
+    editor: SharedPreferences.Editor,
+    key: String,
+    value: Any?,
+) {
+    when (value) {
+        is String -> editor.putString(key, value).apply()
+        is Int -> editor.putInt(key, value).apply()
+        is Boolean -> editor.putBoolean(key, value).apply()
+    }
 }
+
+fun getPref(
+    preferences: SharedPreferences,
+    key: String,
+    value: Any?,
+): Any? =
+    when (value) {
+        is String -> preferences.getString(key, value)
+        is Int -> preferences.getInt(key, value)
+        else -> preferences.getBoolean(key, value as Boolean)
+    }
