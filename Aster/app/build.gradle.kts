69a0fd2e441ca705e63e3e5f899aa70b8d9300d4
uielPark <parkuiery@gmail.com>
2023-12-25 22:30:38 +0900

    ⚙️ :: 컴파일 버전 변경

--- Aster/app/build.gradle.kts
+++ Aster/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.aster.android"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.aster.android"
@@ -42,9 +42,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-runtime:8.2.0")
     testImplementation("junit:junit:4.13.2")
