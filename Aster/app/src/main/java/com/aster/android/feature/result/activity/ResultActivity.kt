7580543ea897c6697e0dc4ac4c7f54d9fee4d464
uielPark <parkuiery@gmail.com>
2023-12-27 08:18:04 +0900

    🌱 :: 파일 추가

---
+++ Aster/app/src/main/java/com/aster/android/feature/result/activity/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.aster.android.feature.result.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aster.android.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
