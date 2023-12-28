b305b6c347d05793241704b3667a56ffb57b91e8
uielPark <parkuiery@gmail.com>
2023-12-28 22:38:55 +0900

    🌱 :: api 파일 추가

--- Aster/app/src/main/java/com/aster/android/network/ApiProvider.kt
+++ Aster/app/src/main/java/com/aster/android/network/ApiProvider.kt
@@ -1,4 +1,45 @@
 package com.aster.android.network
 
-class ApiProvider {
+val authApi : AuthApi by lazy {
+    RetrofitClient.getRetrofit().create(AuthApi::class.java)
+}
+
+val usersApi : UsersApi by lazy {
+    RetrofitClient.getRetrofit().create(UsersApi::class.java)
+}
+
+val userApi : UserApi by lazy {
+    RetrofitClient.getRetrofit().create(UserApi::class.java)
+}
+
+val feedApi : FeedApi by lazy {
+    RetrofitClient.getRetrofit().create(FeedApi::class.java)
+}
+
+val imageApi : ImageApi by lazy {
+    RetrofitClient.getRetrofit().create(ImageApi::class.java)
+}
+
+val surveyStorageApi : SurveyStorageApi by lazy {
+    RetrofitClient.getRetrofit().create(SurveyStorageApi::class.java)
+}
+
+val commentApi : CommentApi by lazy {
+    RetrofitClient.getRetrofit().create(CommentApi::class.java)
+}
+
+val coCommentApi : CoCommentApi by lazy {
+    RetrofitClient.getRetrofit().create(CoCommentApi::class.java)
+}
+
+val schoolClassApi : SchoolClassApi by lazy {
+    RetrofitClient.getRetrofit().create(SchoolClassApi::class.java)
+}
+
+val likeApi : LikeApi by lazy {
+    RetrofitClient.getRetrofit().create(LikeApi::class.java)
+}
+
+val commentLikeApi : CommentLikeApi by lazy {
+    RetrofitClient.getRetrofit().create(CommentLikeApi::class.java)
 }
