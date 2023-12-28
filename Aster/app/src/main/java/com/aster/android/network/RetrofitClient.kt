67e22865df618b08feffa582f01e04df7deed80d
uielPark <parkuiery@gmail.com>
2023-12-28 20:12:40 +0900

    🌱 :: retrofit 세팅

--- Aster/app/src/main/java/com/aster/android/network/RetrofitClient.kt
+++ Aster/app/src/main/java/com/aster/android/network/RetrofitClient.kt
@@ -1,4 +1,47 @@
 package com.aster.android.network
 
-class RetrofitClient {
+import com.aster.android.BuildConfig
+import com.aster.android.util.ACCESS_TOKEN
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private lateinit var retrofit: Retrofit
+
+    fun getRetrofit(): Retrofit {
+        retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor(
+                        RequestInterceptor()
+                    )
+                    .build()
+            )
+            .build()
+        return retrofit
+    }
+}
+
+class RequestInterceptor : Interceptor {
+    private lateinit var request: Request
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val ignorePath = arrayListOf(
+            "/"
+        )
+        request = if (ignorePath.contains(chain.request().url().encodedPath()))
+            chain.request().newBuilder().build()
+        else
+            chain.request().newBuilder().addHeader(
+                "Authorization",
+                "Bearer $ACCESS_TOKEN",
+            ).build()
+
+        return chain.proceed(request)
+    }
 }
