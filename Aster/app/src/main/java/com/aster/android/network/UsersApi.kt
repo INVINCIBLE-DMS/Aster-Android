f1112330f15b6682cc2c2487fae8b206d1dcc052
uielPark <parkuiery@gmail.com>
2023-12-28 22:36:43 +0900

    🌱 :: api 코드 추가

--- Aster/app/src/main/java/com/aster/android/network/UsersApi.kt
+++ Aster/app/src/main/java/com/aster/android/network/UsersApi.kt
@@ -1,14 +1,24 @@
 package com.aster.android.network
 
+import com.aster.android.feature.login.model.AccountXquareResponse
 import com.aster.android.feature.login.model.LoginXquareRequest
 import com.aster.android.feature.login.model.LoginXquareResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.Path
 
-interface SignupApi {
+interface UsersApi {
     @POST("users/login") //스퀘어 로그인
-    suspend fun login_xquare(
+    fun login_xquare(
         @Body loginXquareRequest: LoginXquareRequest,
     ): Response<LoginXquareResponse>
+
+    @GET("users/account-id/{accountId}") //스퀘어 유저 조회
+    fun searchUser_xquare(
+        @Header("Authorization") accessToken: String,
+        @Path("accountId") accountId: String,
+    ): Response<AccountXquareResponse>
 }
