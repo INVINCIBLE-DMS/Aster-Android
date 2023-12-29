14a45d1d82fedf967ad714b9df446e7edba36885
uielPark <parkuiery@gmail.com>
2023-12-29 19:57:49 +0900

    🌱 :: 피드 api

--- Aster/app/src/main/java/com/aster/android/feature/feed/model/FeedResponse.kt
+++ Aster/app/src/main/java/com/aster/android/feature/feed/model/FeedResponse.kt
@@ -1,3 +1,12 @@
 package com.aster.android.feature.feed.model
 
-data class FeedResponse()
+import java.time.LocalDateTime
+
+data class FeedResponse(
+    val id: Long,
+    val content: String,
+    val feedImgUrl: String,
+    val createdAt: LocalDateTime,
+    val likeCount: Int,
+    val isLiked: Boolean,
+)
