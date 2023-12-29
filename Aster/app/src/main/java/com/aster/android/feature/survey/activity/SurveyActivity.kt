a26378f82fe8d276342bd42c082b7453ab694545
uielPark <parkuiery@gmail.com>
2023-12-29 15:15:53 +0900

    🌱 :: 파일 추가

---
+++ Aster/app/src/main/java/com/aster/android/feature/survey/activity/SurveyActivity.kt
@@ -0,0 +1,12 @@
+package com.aster.android.feature.survey.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aster.android.R
+
+class SurveyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_survey)
+    }
+}
