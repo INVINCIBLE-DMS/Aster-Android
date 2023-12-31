51e4a32c8001f28b7d20f237aa2a9b16c28a9f8c
uielPark <parkuiery@gmail.com>
2023-12-31 22:25:31 +0900

    🌱 :: 마이 페이지

--- Aster/app/src/main/java/com/aster/android/feature/myPage/fragment/MyPageFragment.kt
+++ Aster/app/src/main/java/com/aster/android/feature/myPage/fragment/MyPageFragment.kt
@@ -1,60 +1,65 @@
 package com.aster.android.feature.myPage.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.aster.android.R
+import com.aster.android.base.BaseFragment
+import com.aster.android.databinding.FragmentMyPageBinding
+import com.aster.android.feature.myPage.model.MyPageResponse
+import com.aster.android.feature.myPage.repository.MyPageRepository
+import com.aster.android.feature.myPage.viewmodel.MyPageViewModel
+import com.aster.android.feature.myPage.viewmodel.factory.MyPageViewModelFactory
+import com.aster.android.util.OK
+import com.bumptech.glide.Glide
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val myPageRepository: MyPageRepository by lazy {
+        MyPageRepository()
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private val myPageViewModelFactory: MyPageViewModelFactory by lazy {
+        MyPageViewModelFactory(myPageRepository,editor)
+    }
+
+    private val myPageViewModel : MyPageViewModel by lazy {
+        ViewModelProvider(this,myPageViewModelFactory)[MyPageViewModel::class.java]
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.myPageFragment = this
+        binding.myPageViewModel = myPageViewModel
+        initData()
+        observeGetMyInfo()
+    }
+
+    private fun initData() {
+        myPageViewModel.getMyInfo()
+    }
+
+    private fun observeGetMyInfo() {
+        myPageViewModel.myPageResponse.observe(viewLifecycleOwner) {
+            when(it.code()) {
+                OK-> {
+                    setData(it.body()!!)
+                }
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+    private fun setData(data: MyPageResponse) {
+        val candyCount = data.classCandyCount
+        binding.tvMyPageName.text = data.username
+        binding.tvMyPageGrade.text = data.studentId
+        binding.ivMyPageProfile
+        Glide
+            .with(requireContext())
+            .load(data.profileImageUrl)
+            .into(binding.ivMyPageProfile)
+        binding.tvMyPageCandyCount.text = "$candyCount 개"
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyPageFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
