40f12b779b4dd3977810bf21fe8ae0088dbccc19
uielPark <parkuiery@gmail.com>
2023-12-31 21:44:41 +0900

    🌱 :: 회원 가입 api

--- Aster/app/src/main/java/com/aster/android/network/AuthApi.kt
+++ Aster/app/src/main/java/com/aster/android/network/AuthApi.kt
@@ -1,4 +1,15 @@
 package com.aster.android.network
 
+import com.aster.android.feature.login.model.SignupRequest
+import com.aster.android.feature.login.model.SignupResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface AuthApi {
+
+    @POST("/aster/auth/")
+    suspend fun postSignup(
+        @Body request: SignupRequest
+    ): Response<SignupResponse>
 }
