2014b186a92068f390671c47beae92a95198ccdb
uielPark <parkuiery@gmail.com>
2023-12-31 22:06:29 +0900

    🌱 :: 마이페이지 추가

--- Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/factory/MyPageViewModelFactory.kt
+++ Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/factory/MyPageViewModelFactory.kt
@@ -1,4 +1,18 @@
 package com.aster.android.feature.myPage.viewmodel.factory
 
-class MyPageViewModelFactory {
+import android.content.SharedPreferences.Editor
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.aster.android.feature.feed.viewmodel.FeedViewModel
+import com.aster.android.feature.myPage.repository.MyPageRepository
+
+@Suppress("UNCHECKED_CAST")
+class MyPageViewModelFactory(
+    private val myPageRepository: MyPageRepository,
+    private val editor: Editor,
+) : ViewModelProvider.Factory{
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return FeedViewModel(myPageRepository,editor) as T
+    }
 }
