7a66a93ccb893a2961891e89d32f146d4522a0b8
uielPark <parkuiery@gmail.com>
2023-12-31 22:25:43 +0900

    🌱 :: 마이 페이지

--- Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/MyPageViewModel.kt
+++ Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/MyPageViewModel.kt
@@ -1,4 +1,26 @@
 package com.aster.android.feature.myPage.viewmodel
 
-class MyPageViewModel {
+import android.content.SharedPreferences.Editor
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aster.android.feature.myPage.model.MyPageResponse
+import com.aster.android.feature.myPage.repository.MyPageRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class MyPageViewModel(
+    private val myPageRepository: MyPageRepository,
+    private val editor: Editor,
+) :ViewModel(){
+    private val _myPageResponse: MutableLiveData<Response<MyPageResponse>> = MutableLiveData()
+    val myPageResponse : LiveData<Response<MyPageResponse>> = _myPageResponse
+
+    fun getMyInfo() {
+        viewModelScope.launch(Dispatchers.IO) {
+            _myPageResponse.postValue(myPageRepository.getMyInfo())
+        }
+    }
 }
