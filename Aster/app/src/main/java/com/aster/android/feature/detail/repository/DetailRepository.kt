cdadcfec1305d7ae35a9a3d00b4c298bbfbff673
uielPark <parkuiery@gmail.com>
2023-12-31 20:55:29 +0900

    ☁️ :: 토큰 추가

--- Aster/app/src/main/java/com/aster/android/feature/detail/repository/DetailRepository.kt
+++ Aster/app/src/main/java/com/aster/android/feature/detail/repository/DetailRepository.kt
@@ -2,10 +2,11 @@ package com.aster.android.feature.detail.repository
 
 import com.aster.android.feature.detail.model.CommentListResponse
 import com.aster.android.network.commentApi
+import com.aster.android.util.ACCESS_TOKEN
 import retrofit2.Response
 
 class DetailRepository {
     suspend fun getDetail(
         feedId:Long,
-    ): Response<CommentListResponse> = commentApi
+    ): Response<CommentListResponse> = commentApi.getCommentList(ACCESS_TOKEN,feedId)
 }
