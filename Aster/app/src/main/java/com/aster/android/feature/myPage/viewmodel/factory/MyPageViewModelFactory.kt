fca5320d91e36b29d7a9a3ed8615b2148ea8b85a
uielPark <parkuiery@gmail.com>
2023-12-31 22:19:01 +0900

    ☁️ :: 코드 수정

--- Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/factory/MyPageViewModelFactory.kt
+++ Aster/app/src/main/java/com/aster/android/feature/myPage/viewmodel/factory/MyPageViewModelFactory.kt
@@ -3,8 +3,8 @@ package com.aster.android.feature.myPage.viewmodel.factory
 import android.content.SharedPreferences.Editor
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.aster.android.feature.feed.viewmodel.FeedViewModel
 import com.aster.android.feature.myPage.repository.MyPageRepository
+import com.aster.android.feature.myPage.viewmodel.MyPageViewModel
 
 @Suppress("UNCHECKED_CAST")
 class MyPageViewModelFactory(
@@ -13,6 +13,6 @@ class MyPageViewModelFactory(
 ) : ViewModelProvider.Factory{
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return FeedViewModel(myPageRepository,editor) as T
+        return MyPageViewModel(myPageRepository,editor) as T
     }
 }
