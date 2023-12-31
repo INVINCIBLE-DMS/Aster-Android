e5386ccd9a4d360fa9969a7e20b154842d45c8a1
uielPark <parkuiery@gmail.com>
2023-12-31 21:24:18 +0900

    🌱 :: 상세 보기

--- Aster/app/src/main/java/com/aster/android/feature/detail/viewmodel/DetailViewModel.kt
+++ Aster/app/src/main/java/com/aster/android/feature/detail/viewmodel/DetailViewModel.kt
@@ -1,4 +1,26 @@
 package com.aster.android.feature.detail.viewmodel
 
-class DetailViewModel {
+import android.content.SharedPreferences.Editor
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aster.android.feature.detail.model.CommentListResponse
+import com.aster.android.feature.detail.repository.DetailRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class DetailViewModel(
+    private val detailRepository: DetailRepository,
+    private val editor: Editor,
+) : ViewModel(){
+    private val _commentListResponse: MutableLiveData<Response<CommentListResponse>> = MutableLiveData()
+    val commentListResponse: LiveData<Response<CommentListResponse>> = _commentListResponse
+
+    fun getCommentList(feedId: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            _commentListResponse.postValue(detailRepository.getDetail(feedId))
+        }
+    }
 }
