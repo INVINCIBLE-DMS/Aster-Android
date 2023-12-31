761f96d54afc89d4c01a562766faa13c009c0941
uielPark <parkuiery@gmail.com>
2023-12-31 19:00:33 +0900

    🌱 :: user api 추가

--- Aster/app/src/main/java/com/aster/android/network/UserApi.kt
+++ Aster/app/src/main/java/com/aster/android/network/UserApi.kt
@@ -1,4 +1,22 @@
 package com.aster.android.network
 
+import com.aster.android.feature.home.model.MatchingRequest
+import com.aster.android.feature.home.model.MatchingResponse
+import com.aster.android.feature.myPage.model.MyPageResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+
 interface UserApi {
+    @GET("/aster/user/my-info")
+    suspend fun getMyPage(
+        @Header("Authorization") accessToken: String,
+    ): Response<MyPageResponse>
+
+    @GET("/aster/user/")
+    suspend fun getMatching(
+        @Header("Authorization") accessToken: String,
+        @Body requset: MatchingRequest,
+    ): Response<MatchingResponse>
 }
