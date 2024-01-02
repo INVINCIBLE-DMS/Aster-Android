8f6a469b3a9dc1aea6a7f856f13c0edfb5956bf3
uielPark <parkuiery@gmail.com>
2024-01-03 01:27:56 +0900

    🐛 :: 토큰 에러

--- Aster/app/src/main/java/com/aster/android/feature/myPage/repository/MyPageRepository.kt
+++ Aster/app/src/main/java/com/aster/android/feature/myPage/repository/MyPageRepository.kt
@@ -2,11 +2,10 @@ package com.aster.android.feature.myPage.repository
 
 import com.aster.android.feature.myPage.model.MyPageResponse
 import com.aster.android.network.userApi
-import com.aster.android.util.ACCESS_TOKEN
 import retrofit2.Response
 
 class MyPageRepository {
 
     suspend fun getMyInfo(
-    ): Response<MyPageResponse> = userApi.getMyPage(ACCESS_TOKEN)
+    ): Response<MyPageResponse> = userApi.getMyPage()
 }
