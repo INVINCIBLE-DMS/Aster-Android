3b997f49e4976dbbf4c50761028acd4c4ab3864a
uielPark <parkuiery@gmail.com>
2024-01-03 01:26:34 +0900

    🌱 :: 홈 메칭

--- Aster/app/src/main/java/com/aster/android/feature/home/adapter/HomeViewPagerAdapter.kt
+++ Aster/app/src/main/java/com/aster/android/feature/home/adapter/HomeViewPagerAdapter.kt
@@ -1,4 +1,24 @@
 package com.aster.android.feature.home.adapter
 
-class HomeViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.aster.android.feature.home.fragment.MatchBothFragment
+import com.aster.android.feature.home.fragment.MatchFragment
+
+class HomeViewPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+    val fragments : List<Fragment>
+    init {
+        fragments = listOf(MatchFragment(),MatchBothFragment())
+    }
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+
+
 }
