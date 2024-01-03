5e1f9ed217acfa6c854338f4788cc5943836083b
uielPark <parkuiery@gmail.com>
2024-01-03 22:30:25 +0900

    🌱 :: 상세보기

--- Aster/app/src/main/java/com/aster/android/feature/detail/activity/DetailActivity.kt
+++ Aster/app/src/main/java/com/aster/android/feature/detail/activity/DetailActivity.kt
@@ -1,14 +1,17 @@
 package com.aster.android.feature.detail.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
 import com.aster.android.R
 import com.aster.android.base.BaseActivity
 import com.aster.android.databinding.ActivityDetailBinding
+import com.aster.android.feature.detail.model.FeedResponse
 import com.aster.android.feature.detail.repository.DetailRepository
 import com.aster.android.feature.detail.viewmodel.DetailViewModel
 import com.aster.android.feature.detail.viewmodel.factory.DetailViewModelFactory
 import com.aster.android.util.OK
+import com.bumptech.glide.Glide
 
 class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_detail) {
 
@@ -28,8 +31,15 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
         super.onCreate(savedInstanceState)
 
         binding.lifecycleOwner = this
+        initBack()
         initData()
         observeCommentList()
+    }
+
+    private fun initBack() {
+        binding.ivDetailBack.setOnClickListener {
+            finish()
+        }
     }
 
     private fun initData() {
@@ -45,9 +55,35 @@ class DetailActivity : BaseActivity<ActivityDetailBinding>(R.layout.activity_det
             when(it.code()) {
                 OK-> {
 
+                    setFeedData(it.body()!!.feedResponse)
                 }
             }
         }
 
+    }
+
+    @SuppressLint("ResourceAsColor")
+    private fun setFeedData(data: FeedResponse) {
+        binding.apply {
+            Glide
+                .with(baseContext)
+                .load(data.profileImgUrl)
+                .into(ivDetailProfile)
+            tvDetailWriter.text = data.username
+            tvDetailDate.text = data.createdAt
+            tvDetailTitle.text = data.title
+            tvDetailContent.text = data.content
+            btnDetailLike.apply {
+                text = "${data.likeCount}개"
+                if(data.liked) {
+                    setBackgroundColor(R.color.strong_pink)
+                    setBackgroundResource(R.drawable.feed_like_activation)
+
+                }else {
+                    setBackgroundColor(R.color.light_gray)
+                    setBackgroundResource(R.drawable.feed_like_inactive)
+                }
+            }
+        }
     }
 }
