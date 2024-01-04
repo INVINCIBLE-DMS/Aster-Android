b6280662b6bd44427f7df0822fcb11c85f8c0739
uiel <parkuiery@gmail.com>
2024-01-04 13:38:08 +0900

    🌱 :: 좋아요 api

--- Aster/app/src/main/java/com/aster/android/network/LikeApi.kt
+++ Aster/app/src/main/java/com/aster/android/network/LikeApi.kt
@@ -1,4 +1,13 @@
 package com.aster.android.network
 
+import retrofit2.Response
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface LikeApi {
+
+    @POST("/aster/like/{feedId}")
+    suspend fun postLike(
+        @Path("feedId") feedId: Long,
+    ): Response<Void>
 }
